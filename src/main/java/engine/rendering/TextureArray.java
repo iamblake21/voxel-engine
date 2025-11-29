4c17b42a9753fbb417dbf374cafb389c78019a86
Salvatore Liccardo <s.liccardo022@gmail.com>
2025-11-29 13:39:23 +0100

    Fix mipmap settings

--- src/main/java/engine/rendering/TextureArray.java
+++ src/main/java/engine/rendering/TextureArray.java
@@ -81,8 +81,8 @@ public class TextureArray {
         textureId = glGenTextures();
         glBindTexture(GL_TEXTURE_2D_ARRAY, textureId);
 
-        glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAG_FILTER, GL_NEAREST); 
         glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
         glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
